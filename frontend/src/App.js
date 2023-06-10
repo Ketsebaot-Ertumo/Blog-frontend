869bb45e1cb496e437eb53c8fb726a7842b6d6ff
unknown <ertumoketsebaot@gmail.com>
2023-06-10 14:22:39 -0700

    'init'

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+
+const App = () => {
+  return (
+    <>
+        <h1>Blog App</h1>
+    </>
+  );
+}
+
+export default App;
